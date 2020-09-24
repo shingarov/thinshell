1f69f6d4f530c82bf3cafd5ac46f746d26fb4917
Boris Shingarov <shingarov@labware.com>
2020-09-24 18:46:07 -0400

    ARM: Add static print() and exit() code for syscall

--- squeak/arm/squeak.s
+++ squeak/arm/squeak.s
@@ -15,3 +15,23 @@
 .type	_start, %function
 _start:
     bkpt
+
+.type exit, %function
+exit:
+    mov R7, #1
+    mov r0, #42
+    svc 0
+
+.type print, %function
+print:
+    mov r7, #4
+    mov r0, #1
+    mov r1, r5
+    mov r2, #20
+    svc 0
+    mov pc, lr
+
+.type s, %object
+.size s, 20
+s:
+.ascii	"****** TEST ******\012\000"
