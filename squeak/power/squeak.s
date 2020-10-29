126bf5d3151ac13fa8d7360b2dbc52a9c2b1b5ba
Boris Shingarov <shingarov@labware.com>
2020-10-29 13:57:44 -0400

    POWER: Thinshell side of CogILTest>>testCallFull

--- squeak/power/squeak.s
+++ squeak/power/squeak.s
@@ -17,3 +17,33 @@
 .type	_start, %function
 _start:
     trap
+
+
+
+# Stuff below this line is just some doodles to play around with branching
+# to non-JIT-produced code.  This is NOT needed for simulating Cog.
+# See CogILTest>>testCallFull.
+
+.globl exit
+.type exit, %function
+exit:
+    li 0, 1  # exit()
+    li 3, 42
+    sc
+
+.globl print
+.type print, %function
+# Print the string pointed to by GPR5, 20 bytes of it
+print:
+    li 0, 4  # write()
+    li 3, 1  # stdout
+    lis 4, s@ha
+    la 4, s@l(4)
+    li 5, 20
+    sc
+    blr
+    
+.type s, %object
+.size s, 20
+s:
+.ascii  "****** TEST ******\012\000"
